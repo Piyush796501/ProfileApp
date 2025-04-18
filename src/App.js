fcc27c2877b7f33cf07db37768996518cce81013
Piyush lohani <lohanipiyush15@gmail.com>
2025-04-18 21:38:34 +0530

    initial implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
-
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+import HomePage from './Pages/HomePage';
+import Profile from './Pages/Profile';
+import CoursePage from './Pages/CoursePage';
 function App() {
   return (
-    <div className="App">
-     <h1>my website</h1>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>|
+        <Link to='/profile'>Profile</Link>|
+        <Link to='/courses'>Courses</Link>
+      </nav>
+      <Routes>
+        
+        
+      </Routes>
+    </Router>
   );
 }
 
